27c59a214c8817d4d71ebf3f13bc861f90cad875
Forpee <khalilhassam3@gmail.com>
2022-06-04 19:34:17 +0200

    a

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,5 +6,9 @@ void main()
 {
     vec3 col1=vec3(.53,.8,.74);
     vec3 col2=vec3(.2,.256,.61);
+    float pi=3.14159265358979323846264;
+    float fline=sin(vUv.y*6.*pi);
+    float threshold=.005;
+    
     gl_FragColor=vec4(vUv,1.,1.);
 }
